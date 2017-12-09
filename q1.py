bca2e747edbe2ad2a141c6171cbf09ce70ef6d0a
hrstic <hrstic@kth.se>
2017-12-09 21:35:35 +0100

    logLikelihood function + Weights

--- q1.py
+++ q1.py
@@ -19,21 +19,58 @@ def readData():
 
     return y, params
 
+#TODO rewrite code for question 5
+def generateXt():
+    T = 500
+    xt = np.zeros(T+1)
+    xt[0] = np.random.normal(0,np.sqrt(params[1] ** 2 / (1 - params[0] ** 2)))
+    for t in range(1, T + 1):
+        xt[t] = np.random.normal(params[0] * xt[t - 1], params[1])
+    return xt
+
+def gauss_logL(y, V, n, sigma, mu):
+    """Equation 5.57: gaussian likelihood"""
+    return (-(n + 1) * np.log(sigma)
+            - 0.5 * n * ((y - mu) ** 2 + V) / sigma ** 2)
+
+def logLikelihood2(xt,yt, params):
+    wt = np.zeros(len(yt))
+    y_var = params[2]**2 * np.exp(xt[0])
+    #y_mu = 0
+    wt[0] = -1/2 * len(yt) * np.log(2 * np.pi * y_var) - ((yt[0])**2)/(2 * y_var)
+    for t in range(1,len(yt)):
+        y_var = params[2]**2 * np.exp(xt[t])
+        wt[t] = (term1 - term2)*wt[t-1]
+        print(wt[t])
+    return wt
+
+
+def logLikelihood(xt,yt, params):
+    wt = np.zeros(len(yt))
+    y_var = params[2]**2 * np.exp(xt[0])
+    #y_mu = 0
+    #Mean mu is equal to zero and that is why it is not included in wt[]
+    #wt[0] = -1/2 * np.log(2 * np.pi * y_var) - ((yt[0])**2)/(2 * y_var)
+    wt[0] = 1
+    for t in range(1,len(yt)):
+        y_var = params[2]*params[2] * np.exp(xt[t])
+        term1 = -1/2 * len(yt[:t]) * np.log(2 * np.pi * y_var)
+        sum_term = 0
+        for i in range(t+1):
+            sum_term = sum_term + (yt[i]**2)/(2*y_var)
+        #normalizing weights
+        wt[t] = (term1 - sum_term)
+        #print(term1 - sum_term)
+        #wt[t] = wt[t] / np.sum(wt)
+    #wt += 1.e-300 # avoid round-off to zero
+    wt /= np.sum(wt)
+    return wt
+
 yt, params = readData()
-
-print(len(y))
-T = 500
-xt = np.zeros(T+1)
-print(len(xt))
-xt[0] = np.random.normal(0,np.sqrt(params[1] ** 2 / (1 - params[0] ** 2)))
-for t in range(1, T + 1):
-    xt[t] = np.random.normal(params[0] * xt[t - 1], params[1])
-#plt.plot(x[1:501],y)
-
-wt = np.zeros(500)
-wt = y/xt[1:501]
-#print(wt)
-
-t = np.linspace(1,501, 500)
-plt.plot(t, y,'+')
+xt = generateXt()
+print(xt)
+print(yt)
+wt = logLikelihood(xt, yt, params)
+print(np.sum(wt))
+plt.plot(np.linspace(0,1,500), wt,'*')
 plt.show()
