a1f58f6aed235c64bbf28bc947e6b83f1de72cde
hrstic <hrstic@kth.se>
2017-12-10 19:21:43 +0100

    Calculating the SMC - now it should be legit

--- q1.py
+++ q1.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.stats
 
 #Read the data from the file and put it in the datastructures
 def readData():
@@ -29,29 +29,34 @@ def generateXt():
         xt[t] = np.random.normal(params[0] * xt[t - 1], params[1])
     return xt
 
-def calculateWeights(xt,yt, params):
-    wt = np.zeros(len(yt))
-    y_var = params[2]**2 * np.exp(xt[0])
-    #y_mu = 0
-    #Mean equals zero and that is why it is not included in the calculations
-    wt[0] = -1/2 * np.log(2 * np.pi * y_var) - ((yt[0])**2)/(2 * y_var)
+def calculateSMC(xt,yt, params, n_particles):
+    wt = np.ones((len(xt), n_particles))
+    wt[0] = wt[0]/n_particles
+    #Instantiate weights randomly across 100 particles
+    for i in range(n_particles):
+        y_var = params[2]**2 * np.exp(xt[0])
+        wt[0][i] = np.random.uniform(1,n_particles)
+    wt[0] /= np.sum(wt[0])
+    #Iterate in time t, calculate weights w.r.t previous value and likelihood
     for t in range(1,len(yt)):
-        y_var = params[2]*params[2] * np.exp(xt[t])
-        term1 = -1/2 * len(yt[:t]) * np.log(2 * np.pi * y_var)
-        sum_term = 0
-        for i in range(t+1):
-            sum_term = sum_term + (yt[i]**2)/(2*y_var)
-        wt[t] = (term1 - sum_term)
-    #normalizing weights
-    wt /= np.sum(wt)
+        likelihood = scipy.stats.multivariate_normal.pdf(xt[t],0, y_var)
+        for i in range(n_particles):
+            y_var = params[2]**2 * np.exp(xt[t])
+            wt[t][i] = likelihood * wt[t-1][i]
+        #in order to normalize we need to divide and thereby we give som values to zeros
+        wt[t] += 1.e-300
+        wt[t] /= np.sum(wt[t])
     return wt
 
 yt, params = readData()
 #We need x in order to calculate the variance for yt
 xt = generateXt()
-print(xt)
-print(yt)
-wt = calculateWeights(xt, yt, params)
-print("Weights sum"np.sum(wt))
-plt.plot(np.linspace(0,1,500), wt,'*')
+n_particles = 100
+wt = calculateSMC(xt, yt, params, n_particles)
+max_weight = np.max(wt[2])
+particle = np.where(wt[2] == max_weight)[0][0]
+print("Max weight ",max_weight, " belongs to particle ", particle)
+#print(wt)
+print("Weights sum:",np.sum(wt[2]))
+plt.plot(np.linspace(1,100,n_particles), wt[2], '*')
 plt.show()
